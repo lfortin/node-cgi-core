d4137b2627eefa9f2cd6ecb5bf2d8b4237092fd8
Laurent Fortin <laurent.fortin@gmail.com>
2024-08-25 21:51:52 -0400

    added sample cgi scripts

---
+++ cgi-bin/env-text.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+# Copyright (c) 2024 lfortin
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+print "Content-Type: text/plain\n\n";
+
+print "<!doctype html>\n";
+print "<html>\n";
+print "
+<style>
+
+</style>
+";
+
+print "<body>\n";
+
+foreach my $key (keys %ENV) {
+  print "$key: $ENV{$key}<br/>\n";
+}
+
+print "</body>\n";
+print "</html>\n";
+
+1;
