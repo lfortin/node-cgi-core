5b6b1b5548f648fb1ca64268b51c4a0c31c57eb8
Laurent Fortin <laurent.fortin@gmail.com>
2024-08-28 23:36:11 -0400

    improved types

--- cgi-core.d.ts
+++ cgi-core.d.ts
@@ -1,9 +1,14 @@
-declare const defaultExtensions: Extensions;
-declare const defaultConfig: Config;
-declare function createHandler(configOptions?: Partial<Config>): (req: IncomingMessage, res: ServerResponse) => Promise<boolean>;
+import { IncomingMessage, ServerResponse } from 'node:http';
+
+export const defaultExtensions: Extensions;
+export const defaultConfig: Config;
+
+export function createHandler(configOptions?: Partial<Config>): (req: IncomingMessage, res: ServerResponse) => Promise<boolean>;
+
 interface Extensions {
     [key: string]: Array<string>;
 }
+
 interface Config {
     urlPath: string;
     filePath: string;
