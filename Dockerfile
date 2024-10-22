a87be6a14a30faff2c3112337335c8425af85583
Laurent Fortin <laurent.fortin@gmail.com>
2024-10-22 10:53:02 -0400

    improved Dockerfile example

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,15 @@ FROM node:20
 
 WORKDIR /usr/src/app
 
+RUN npm install --save serve-static finalhandler cgi-core
+
 COPY ./cgi-core.js ./
 COPY ./lib/ ./lib/
 COPY ./server/ ./server/
 
+VOLUME ["/usr/src/app/htdocs"]
 VOLUME ["/usr/src/app/cgi-bin"]
 
 EXPOSE 3001
 
-CMD ["node", "server/http.mjs"]
+CMD ["node", "server/docker.mjs"]
---
+++ server/docker.mjs
@@ -0,0 +1,47 @@
+// Copyright (c) 2024 lfortin
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import { createServer } from "node:http";
+import serveStatic from "serve-static";
+import finalhandler from "finalhandler";
+import { createHandler } from "cgi-core";
+
+const port = 3001;
+
+const staticHandler = serveStatic("/usr/src/app/htdocs", {
+  index: ["index.html", "index.htm"],
+});
+
+const cgiHandler = createHandler({
+  filePath: "/usr/src/app/cgi-bin",
+  env: { SERVER_PORT: port, ...process.env },
+});
+
+const app = createServer(async (req, res) => {
+  const requestHandled = await cgiHandler(req, res);
+
+  if (!requestHandled) {
+    staticHandler(req, res, finalhandler(req, res));
+  }
+});
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
+});
